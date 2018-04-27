7ce348bb0ccd757df6e0d2842282098c7bc8ae66
Hamish Lyall <hamishrlyall@gmail.com>
2018-04-27 10:25:29 +1000

    Add project files.

---
+++ Dip_Database_Deployment_Week8/Author.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Author]
+(
+	[authorId] VARCHAR(20) NOT NULL PRIMARY KEY,
+	[firstName] NCHAR(100) NOT NULL,
+	[lastName] NCHAR(100) NOT NULL,
+	[authorTaxFileNumber] INT NOT NULL
+)
---
+++ Dip_Database_Deployment_Week8/Student.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[studentId] VARCHAR(20) NOT NULL PRIMARY KEY,
+	[firstName] NCHAR(100) NOT NULL,
+	[lastName] NCHAR(100) NOT NULL,
+	[email] NCHAR(50) NOT NULL,
+	[mobileNumber] NCHAR(20) NOT NULL
+)
